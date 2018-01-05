9c99ed499fe84fed61adf91dc29df6efa59ff4f3
meduka <31477336+meduka@users.noreply.github.com>
2018-01-05 09:18:32 -0500

    Add files via upload

--- graphics_demo.py
+++ graphics_demo.py
@@ -1,6 +1,7 @@
 # Imports
 import pygame
 import math
+import random
 
 # Initialize game engine
 pygame.init()
@@ -29,7 +30,8 @@ SKY = (4, 18, 40)
 REDD = (255, 0, 1)
 BROWN = (122, 44, 4)
 BROWNNN = (122, 44, 0)
-YELLOW = (255, 255, 175)
+STARS = (255, 255, 175)
+MOON = (255, 250, 107)
 
 
 def draw_cloud(x, y):
@@ -38,6 +40,18 @@ def draw_cloud(x, y):
     pygame.draw.ellipse(screen, WHITE, [x + 20, y + 10, 25, 25])
     pygame.draw.ellipse(screen, WHITE, [x + 35, y, 50, 50])
     pygame.draw.rect(screen, WHITE, [x + 20, y + 20, 60, 40])
+
+def make_stars():
+    for s in stars:
+        pygame.draw.ellipse(screen, STARS, s)
+
+stars = []
+for i in range(200):
+    x = random.randrange(0, 800)
+    y = random.randrange(0, 400)
+    r = random.randrange(1, 5)
+    stars.append([x, y, r, r,])
+
 
 
 
@@ -67,17 +81,21 @@ while not done:
 
     '''
 
-    ''' sun '''
-    pygame.draw.ellipse(screen, YELLOW, [575, 75, 100, 100])
+    ''' moon '''
+    pygame.draw.ellipse(screen, MOON, [575, 75, 100, 100])
 
- 
+  
+
+
+    make_stars()
+     
     '''clouds'''
     draw_cloud(50, 150)
     draw_cloud(250, 75)
-    draw_cloud(250, 75)
-    draw_cloud(250, 75)
-    draw_cloud(250, 75)
-    draw_cloud(250, 75)
+    draw_cloud(150, 200)
+    draw_cloud(400, 100)
+    draw_cloud(540, 200)
+    draw_cloud(100, 20)
     draw_cloud(640, 90)
     draw_cloud(400, 200)
 
@@ -86,12 +104,12 @@ while not done:
     for x in range(5, 600, 90):
         post = [[x+20, y+20], [x+40, y+20], [x+40, y+160], [x, y+160], [x, y+20]]
         pygame.draw.polygon(screen, WHITE, post)
-
+    '''
     pygame.draw.rect(screen, WHITE, [0, y+10, 800, 5])
     pygame.draw.rect(screen, WHITE, [0, y+30, 800, 5])
+    '''
 
-
-
+    
 
     '''
     pygame.draw.ellipse(screen, WHITE, [25, 25, 450, 450])
@@ -100,7 +118,7 @@ while not done:
     '''
 
     ''' angles for arcs are measured in radians (a pre-cal topic) '''
-    pygame.draw.arc(screen, ORANGE, [700, 25, 100, 800], 0, math.pi/2, 50)
+    pygame.draw.arc(screen, BROWN, [700, 25, 100, 800], 0, math.pi/2, 50)
 
 
     # Update screen (Actually draw the picture in the window.)
